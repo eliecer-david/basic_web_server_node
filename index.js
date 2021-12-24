f217d610ea181afc06cd6bff5d5ace37789dc217
Eliecer David <eparialhuay@gmail.com>
2021-12-24 17:56:44 -0500

    serve routes without extension

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const port = 3000;
 
 app.use(express.static("public"))
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/generic", function (req, res) {
+  res.sendFile(__dirname + "/public/generic.html");
+});
+
+app.get("/elements", function (req, res) {
+  res.sendFile(__dirname + "/public/elements.html");
+});
+
 app.get("*", function (req, res) {
   res.status(404);
   res.sendFile(__dirname + "/public/404.html");
