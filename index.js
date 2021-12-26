8b86b998005d3f8d6db75cce1a019f1f94d14e16
Eliecer David <eparialhuay@gmail.com>
2021-12-25 21:36:16 -0500

    define port from env

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
